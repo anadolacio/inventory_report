a600087ad49d599e9e7a2f9e63b08ea376dc3142
anadolacio <anadolacio@hotmail.com>
2023-08-25 19:05:55 -0300

    requisito e1 e 2

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    data = {
+        "id": "1",
+        "product_name": "cadeira",
+        "company_name": "Target Corporation",
+        "manufacturing_date": "2021-02-18",
+        "expiration_date": "2025-09-17",
+        "serial_number": "CR25",
+        "storage_instructions": "empilhas",
+    }
+
+    product = Product(**data)
+
+    assert product.id == "1"
+    assert product.company_name == "Target Corporation"
+    assert product.product_name == "cadeira"
+    assert product.manufacturing_date == "2021-02-18"
+    assert product.expiration_date == "2025-09-17"
+    assert product.serial_number == "CR25"
+    assert product.storage_instructions == "empilhas"
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,26 @@
-def test_product_report() -> None:
-    raise NotImplementedError
+from inventory_report.product import Product
+import pytest
+
+
+@pytest.mark.parametrize(
+    "data, expected_result",
+    [
+        (
+            {
+                "id": "1",
+                "product_name": "cadeira",
+                "company_name": "Target Corporation",
+                "manufacturing_date": "2021-02-18",
+                "expiration_date": "2025-09-17",
+                "serial_number": "CR25",
+                "storage_instructions": "empilhas",
+            },
+            """The product 1 - cadeira with serial number CR25 manufactured on 2021-02-18 by the company Target Corporation valid until 2025-09-17 must be stored according to the following instructions: empilhas.""",
+        ),
+    ],
+)
+def test_product_report(data, expected_result) -> None:
+    # raise NotImplementedError
+    product = Product(**data)
+
+    assert str(product) == str(expected_result)
