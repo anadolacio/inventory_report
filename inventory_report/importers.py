0479736b5106a2e03d002cc9df11978927ef0d11
anadolacio <anadolacio@hotmail.com>
2023-08-25 19:39:39 -0300

    requisito 4

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,8 @@
 from typing import Dict, Type, List
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
+import json
+# import csv
 
 
 class Importer(ABC):
@@ -12,12 +14,54 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+
+    def import_data(self) -> List[Product]:
+        with open(self.path, "r") as json_file:
+            file = json.load(json_file)
+
+        product_list = [
+            Product(
+                id=line["id"],
+                product_name=line["product_name"],
+                company_name=line["company_name"],
+                manufacturing_date=line["manufacturing_date"],
+                expiration_date=line["expiration_date"],
+                serial_number=line["serial_number"],
+                storage_instructions=line["storage_instructions"],
+            )
+            for line in file
+        ]
+
+        return product_list
 
 
-class CsvImporter:
+class CsvImporter(Importer):
     pass
+    # def __init__(self, path: str) -> None:
+    #     super().__init__(path)
+
+    # def import_data(self) -> List[Product]:
+
+    #     with open(self.path, "r") as csv_file:
+    #         file = csv.DictReader(csv_file)
+
+    #     product_list = [
+    #         Product(
+    #             id=line["id"],
+    #             product_name=line["product_name"],
+    #             company_name=line["company_name"],
+    #             manufacturing_date=line["manufacturing_date"],
+    #             expiration_date=line["expiration_date"],
+    #             serial_number=line["serial_number"],
+    #             storage_instructions=line["storage_instructions"],
+    #         )
+    #         for line in file
+    #     ]
+
+    #     return product_list
 
 
 # Não altere a variável abaixo
