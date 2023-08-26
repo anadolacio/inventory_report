d3fedd6fa5de88622b58d604ff9c4957229a8e12
anadolacio <anadolacio@hotmail.com>
2023-08-25 22:32:36 -0300

    requisito 54

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,16 @@
+from inventory_report.product import Product
+from typing import List, Union
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Union[List[Product], None] = None):
+        if data is None:
+            data = []
+        self._data = data
+
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data
