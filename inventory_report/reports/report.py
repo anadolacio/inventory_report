2e60838ff707f0be877375fd5bf7e1edcfac67ee
anadolacio <anadolacio@hotmail.com>
2023-08-25 22:46:22 -0300

    requisito 6

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,13 @@
-class Report:
-    pass
+from inventory_report.inventory import Inventory
+from typing import Protocol
+
+
+class Report(Protocol):
+    def __init__(self) -> None:
+        pass
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        pass
+
+    def generate(self) -> str:
+        return "pass"
