240e0aa3211df553a4809e19df4fd8fca58ee7ae
anadolacio <anadolacio@hotmail.com>
2023-08-31 14:44:58 -0300

    resolução requisito 2 retirando o parametrize

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,51 +1,51 @@
 from inventory_report.product import Product
 
-import pytest
+# import pytest
 
 
-@pytest.mark.parametrize(
-    "data, expected_result",
-    [
-        (
-            {
-                "id": "1",
-                "product_name": "cadeira",
-                "company_name": "Target Corporation",
-                "manufacturing_date": "2021-02-18",
-                "expiration_date": "2025-09-17",
-                "serial_number": "CR25",
-                "storage_instructions": "empilhas",
-            },
-            (
-                "The product 1 - cadeira with serial number CR25 manufactured "
-                "on 2021-02-18 by the company Target Corporation "
-                "valid until 2025-09-17 must be stored according to the "
-                "following instructions: empilhas."
-            ),
-        ),
-    ],
-)
-def test_product_report(data, expected_result) -> str:
-    # raise NotImplementedError
-    product = Product(**data)
+# @pytest.mark.parametrize(
+#     "data, expected_result",
+#     [
+#         (
+#             {
+#                 "id": "1",
+#                 "product_name": "cadeira",
+#                 "company_name": "Target Corporation",
+#                 "manufacturing_date": "2021-02-18",
+#                 "expiration_date": "2025-09-17",
+#                 "serial_number": "CR25",
+#                 "storage_instructions": "empilhas",
+#             },
+#             (
+#                 "The product 1 - cadeira with serial number CR25 manufactured "
+#                 "on 2021-02-18 by the company Target Corporation "
+#                 "valid until 2025-09-17 must be stored according to the "
+#                 "following instructions: empilhas."
+#             ),
+#         ),
+#     ],
+# )
+# def test_product_report(data, expected_result) -> None:
+#     # raise NotImplementedError
+#     product = Product(**data)
 
-    assert str(product) == str(expected_result)
+#     assert str(product) == str(expected_result)
 
 
-# def test_product_report() -> None:
-#     #     # raise NotImplementedError
-#     product = Product(
-#         "1",
-#         "cadeira",
-#         "Target Corporation",
-#         "2021-02-18",
-#         "2025-09-17",
-#         "CR25",
-#         "empilhas",
-#     )
+def test_product_report() -> None:
+    #     # raise NotImplementedError
+    product = Product(
+        "1",
+        "cadeira",
+        "Target Corporation",
+        "2021-02-18",
+        "2025-09-17",
+        "CR25",
+        "empilhas",
+    )
 
-#     assert str(product) == str(
-#         "The product 1 - cadeira with serial number CR25 manufactured on "
-#         "2021-02-18 by the company Target Corporation valid until 2025-09-17 "
-#         "must be stored according to the following instructions: empilhas."
-#     )
+    assert str(product) == str(
+        "The product 1 - cadeira with serial number CR25 manufactured on "
+        "2021-02-18 by the company Target Corporation valid until 2025-09-17 "
+        "must be stored according to the following instructions: empilhas."
+    )
